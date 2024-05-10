e41515701f9ad06f97627fe8b0cebe68c6701fd1
festfgr <dsjcemarcusroby@hotmail.com>
2024-05-10 19:18:38 +0700

    init project

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function isValidBST(root) {
-  let prev = null;
-  return inorder(root);
-  function inorder(node) {
-    if (!node) return true;
-    if (!inorder(node.left)) return false;
-    if (prev !== null && node.val <= prev) return false;
-    prev = node.val;
-    return inorder(node.right);
-  }
+function add(x, y) {
+  return x + y;
 }
+
+const result = add(5, 3);
+console.log(result); // 8
